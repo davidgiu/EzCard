83dac3c77a878af8db2191aefa57ba539a2da602
david <david.giudice@he-arc.ch>
2024-02-05 13:20:34 +0100

    test

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
     //eslint-disable-next-line
-    base: process.env.NODE_ENV === "production" ? "/EzCard/" : "/",
+    base: process.env.NODE_ENV === "production" ? "/ezcard/" : "/",
     
     plugins: [vue()],
     resolve: {
