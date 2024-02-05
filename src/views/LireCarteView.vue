cb491adc810fe6d9f19491d65ea5327aa7924587
david <david.giudice@he-arc.ch>
2024-02-05 20:09:55 +0100

    nfc

--- src/views/LireCarteView.vue
+++ src/views/LireCarteView.vue
@@ -7,29 +7,23 @@
         <div class="row">
             <div class="col-6">
                 <div class="buttons-container d-flex flex-column align-items-start" v-if="!decodedUser">
-                    <!-- Boutons à gauche -->
                     <div class="d-flex flex-column align-items-center mb-5">
                         <button class="btn btn-success mt-5 large-button" @click="demarrerCamera">Caméra</button>
-
                         <button class="btn btn-success mt-5 large-button" @click="importerQRCode">
                             Importer depuis les fichiers
                         </button>
-
-                        <!-- Bouton NFC -->
                         <button class="btn btn-success mt-5 large-button" @click="lireNFC">Lire via NFC</button>
                     </div>
                 </div>
             </div>
             <div class="col-6 mt-5">
                 <div class="buttons-container d-flex flex-column align-items-center">
-                    <!-- Boutons à droite -->
                     <div class="d-flex flex-column align-items-center">
                         <QrcodeStream
                             v-if="cameraActive && !decodedUser && !capturing"
                             @decode="onDecode"
                             style="max-width: 80%"
                         />
-
                         <button
                             class="btn btn-success small-button"
                             v-if="cameraActive && !decodedUser && !capturing"
@@ -38,13 +32,11 @@
                             Capture Photo
                         </button>
                     </div>
-
                     <input type="file" ref="fileInput" @change="handleFileChange" style="display: none" />
                 </div>
             </div>
             <div class="col-12">
                 <div class="content-container d-flex flex-column align-items-end" v-if="decodedUser">
-                    <!-- Centrer la carte et le bouton Enregistrer -->
                     <button class="btn btn-success mb-5 large-button" @click="enregistrerCarte">
                         Enregistrer la Carte
                     </button>
@@ -59,22 +51,11 @@
         </div>
     </div>
 
-    <div v-if="erreurMessage" class="alert alert-danger mt-3" role="alert">
-        {{ erreurMessage }}
-    </div>
+    <div v-if="erreurMessage" class="alert alert-danger mt-3" role="alert">{{ erreurMessage }}</div>
 </template>
 
 <script>
-import { NDEFReader } from "nfc";
-import jsQR from "jsqr";
-import { QrcodeStream } from "vue-qrcode-reader";
-import cardComponent from "@/components/cardComponent.vue";
-
 export default {
-    components: {
-        QrcodeStream,
-        cardComponent
-    },
     data() {
         return {
             decodedUser: null,
@@ -98,19 +79,15 @@ export default {
                 navigator.mediaDevices
                     .getUserMedia({ video: true })
                     .then((stream) => {
-                        // Capture an image from the video stream
                         const video = document.createElement("video");
                         video.srcObject = stream;
                         video.play();
-
                         setTimeout(() => {
                             const canvas = document.createElement("canvas");
                             canvas.width = video.videoWidth;
                             canvas.height = video.videoHeight;
                             const context = canvas.getContext("2d");
                             context.drawImage(video, 0, 0, canvas.width, canvas.height);
-
-                            // Get the image data from the canvas and detect the QR code
                             const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
                             const code = jsQR(imageData.data, imageData.width, imageData.height);
 
@@ -122,10 +99,9 @@ export default {
                                 this.erreurMessage = "Erreur : Aucun QR code trouvé dans l'image.";
                             }
 
-                            // Stop the video stream
                             stream.getTracks().forEach((track) => track.stop());
                             this.capturing = false;
-                        }, 500); // Réduisez le délai à 500 millisecondes ou ajustez selon vos besoins
+                        }, 500);
                     })
                     .catch((error) => {
                         console.error("Erreur lors de l'accès à la caméra :", error);
@@ -135,19 +111,10 @@ export default {
             }
         },
         enregistrerCarte() {
-            // Récupérer les cartes existantes depuis le localStorage
             const cartesExistantes = JSON.parse(localStorage.getItem("cartes")) || [];
-
-            // Ajouter la nouvelle carte
             cartesExistantes.push(this.decodedUser);
-
-            // Enregistrer la liste mise à jour dans le localStorage
             localStorage.setItem("cartes", JSON.stringify(cartesExistantes));
-
-            // Afficher le message de validation
             this.validationMessage = "Carte enregistrée avec succès.";
-
-            // Masquer la caméra et le bouton de capture photo
             this.cameraActive = false;
         },
         importerQRCode() {
@@ -166,7 +133,6 @@ export default {
                         canvas.width = image.width;
                         canvas.height = image.height;
                         ctx.drawImage(image, 0, 0, image.width, image.height);
-
                         const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
                         const code = jsQR(imageData.data, imageData.width, imageData.height);
 
@@ -175,7 +141,6 @@ export default {
                             this.erreurMessage = "";
                         } else {
                             console.error("Aucun QR code trouvé dans l'image.");
-                            // Afficher le message d'erreur
                             this.erreurMessage = "Erreur : Aucun QR code trouvé dans l'image.";
                         }
                     };
@@ -187,16 +152,10 @@ export default {
         lireNFC() {
             if ("NDEFReader" in window) {
                 const reader = new NDEFReader();
-
                 reader.scan().then(
-                    (result) => {
-                        if (result && result.message) {
-                            const decodedData = this.decoderDonneesNFC(result.message);
-                            this.onDecode(decodedData);
-                        } else {
-                            console.error("Aucune donnée NFC trouvée.");
-                            this.erreurMessage = "Erreur : Aucune donnée NFC trouvée.";
-                        }
+                    ({ records }) => {
+                        const decodedData = this.decoderDonneesNFC(records);
+                        this.onDecode(decodedData);
                     },
                     (error) => {
                         console.error("Erreur lors de la lecture NFC :", error);
@@ -208,9 +167,8 @@ export default {
                 this.erreurMessage = "L'API NFC n'est pas prise en charge par votre navigateur.";
             }
         },
-
-        decoderDonneesNFC(message) {
-            const records = message.records.map((record) => {
+        decoderDonneesNFC(records) {
+            const decodedUser = records.reduce((acc, record) => {
                 switch (record.recordType) {
                     case "text":
                         return this.decoderTexte(record);
@@ -218,24 +176,22 @@ export default {
                         console.warn(`Type de record non pris en charge : ${record.recordType}`);
                         return null;
                 }
-            });
-
-            // Retournez les données interprétées directement
-            const decodedUser = records.reduce((acc, record) => {
-                return { ...acc, ...record };
             }, {});
 
-            // Utilisez JSON.parse pour décoder les données JSON
-            if (decodedUser.texte) {
-                const decodedData = JSON.parse(decodedUser.texte); // Assurez-vous d'ajuster selon votre structure
+            if (decodedUser.text) {
+                const decodedData = JSON.parse(decodedUser.text);
                 this.onDecode(decodedData);
-                this.nfcData = decodedData; // Mettez à jour nfcData avec les données décodées
+                this.nfcData = decodedData;
                 return decodedData;
             } else {
                 console.error("Aucune donnée texte trouvée dans le record NFC.");
                 this.erreurMessage = "Erreur : Aucune donnée texte trouvée dans le record NFC.";
                 return null;
             }
+        },
+        decoderTexte(record) {
+            // Ajoutez ici votre logique pour décoder le type "text" du record NFC
+            return { text: record.data };
         }
     }
 };
@@ -243,15 +199,15 @@ export default {
 
 <style>
 .large-button {
-    font-size: 35px; /* Taille de la police */
-    height: 100px; /* Hauteur du bouton */
+    font-size: 35px;
+    height: 100px;
     width: 500px;
     margin-left: 50px;
 }
 
 .small-button {
-    font-size: 20px; /* Taille de la police */
-    height: 50px; /* Hauteur du bouton */
+    font-size: 20px;
+    height: 50px;
     width: 200px;
     margin-left: 50px;
 }
