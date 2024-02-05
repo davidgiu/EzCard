8d241ba804556946f3f8bc5c1dcd2ed2c5f2a1a0
david <david.giudice@he-arc.ch>
2024-02-05 12:40:05 +0100

    balbla

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
     //eslint-disable-next-line
-    base: process.env.NODE_ENV === "production" ? "/projet23-toc-tuc/" : "/",
+    base: process.env.NODE_ENV === "production" ? "/EzCard/" : "/",
     
     plugins: [vue()],
     resolve: {
